7ccfd52f4b6852f4fddcec39e4cdc0ed0e5a6677
Kathy Dieser <katdiese@gmail.com>
2016-06-13 17:45:37 -0600

    accessing api

--- src/components/landing/landing.controller.js
+++ src/components/landing/landing.controller.js
@@ -6,8 +6,20 @@
     .module('weatherApp')
     .controller('LandingCtrl', LandingCtrl);
 
-    function LandingCtrl() {
+    LandingCtrl.$inject = ['weatherService'];
+
+    function LandingCtrl(weatherService) {
       var vm = this;
+
+      vm.getInfo = function() {
+        weatherService.getWeather('Indianapolis')
+        .then(function(data) {
+          console.log(data);
+        })
+      }
+
+      vm.getInfo();
+
     }
 
 })();
