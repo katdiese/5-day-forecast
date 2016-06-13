e245807d5ee2d165a0a27403b996287e3280e9d6
Kathy Dieser <katdiese@gmail.com>
2016-06-13 17:55:23 -0600

    changed API to get temp back in F

--- js/services.js
+++ js/services.js
@@ -13,7 +13,7 @@
     return {
 
       getWeather: function(cityName) {
-        return $http.get('http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + ',us&APPID=3c438ab921656e7189ededb4dbb9bb82');
+        return $http.get('http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + ',us&units=imperial&APPID=3c438ab921656e7189ededb4dbb9bb82');
       }
 
     }
