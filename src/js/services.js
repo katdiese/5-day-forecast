d9abd969d3de5c5bf191bf91f6085a48a6cd7eea
Kathy Dieser <katdiese@gmail.com>
2016-06-13 17:29:33 -0600

    connecting to weather api

---
+++ src/js/services.js
@@ -0,0 +1,23 @@
+(function() {
+
+  'use strict';
+
+  angular
+    .module('weatherApp')
+    .service('weatherService', weatherService);
+
+  weatherService.$inject = ['$http'];
+
+  function weatherService($http) {
+
+    return {
+
+      getWeather: function(cityName) {
+        return $http.get('http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + ',us&APPID=3c438ab921656e7189ededb4dbb9bb82');
+      }
+
+    }
+
+  }
+
+})();
