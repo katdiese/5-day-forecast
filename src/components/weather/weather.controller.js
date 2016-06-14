b440f6ae55b596fa78814cb769965c7fc1b87e98
Kathy Dieser <katdiese@gmail.com>
2016-06-13 18:57:37 -0600

    split up day forecasts

--- src/components/weather/weather.controller.js
+++ src/components/weather/weather.controller.js
@@ -11,10 +11,25 @@
     function WeatherCtrl($rootScope, weatherService) {
       var vm = this;
 
+      vm.cityName;
+      // vm.day1Forecast;
+      // vm.day2Forecast;
+      // vm.day3Forecast;
+      // vm.day4Forecast;
+      // vm.day5Forecast;
+
       vm.getInfo = function() {
         weatherService.getWeather($rootScope.city)
         .then(function(data) {
           console.log(data);
+          vm.cityName = data.data.city.name;
+          var fullForecast = data.data.list;
+          vm.day1Forecast = fullForecast.slice(0,7);
+          vm.day2Forecast = fullForecast.slice(7,15);
+          vm.day3Forecast = fullForecast.slice(15,23);
+          vm.day4Forecast = fullForecast.slice(23,31);
+          vm.day5Forecast = fullForecast.slice(31,39);
+          console.log('day 5 ', vm.day5Forecast);
         })
       }
 
