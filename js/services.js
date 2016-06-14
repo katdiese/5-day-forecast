db953d39e78cd2fc7129d0634a2ff7798b9368bf
Kathy Dieser <katdiese@gmail.com>
2016-06-13 18:29:56 -0600

    added rootscope functionality

--- js/services.js
+++ js/services.js
@@ -6,12 +6,15 @@
     .module('weatherApp')
     .service('weatherService', weatherService);
 
-  weatherService.$inject = ['$http'];
+  weatherService.$inject = ['$http', '$rootScope'];
 
-  function weatherService($http) {
+  function weatherService($http, $rootScope) {
 
     return {
 
+      setCity: function(cityName) {
+        $rootScope.city = cityName;
+      },
       getWeather: function(cityName) {
         return $http.get('http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + ',us&units=imperial&APPID=3c438ab921656e7189ededb4dbb9bb82');
       }
